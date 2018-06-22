2cb16cba9e4dea9b963707c857b86e5715f978ba
Diego Cotrina <dian.cs183@gmail.com>
2018-06-22 15:27:08 -0500

    Modulo 1 terminado

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string getMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string getMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
